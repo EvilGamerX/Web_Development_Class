12a01a6601b89e0ffd9493d175e6b06c89379579
Stephen Gaspar <evilgamerx42@gmail.com>
2013-10-08 19:23:55 -0400

    file indexer v2, get html content of html files in directory and any sub directories
    fixed return character "\r" not being accounted for
    
    meta tags not appearing

--- file_indexer/file_indexer.php
+++ file_indexer/file_indexer.php
@@ -1,42 +1,109 @@
 <?PHP
-	$url = "http://cs465.free.fr/files/hw3b.html";
 	
-	$meta = get_meta_tags($url);
-	
-	foreach($meta as $k=>$item)
+    if(isset($_POST["Directory"]))
+    {
+        $url = $_POST["Directory"];
+    }else
+    {
+        echo "<form action=\"file_indexer.php\" method=\"post\">";
+        echo "<input type=\"text\" name=\"Directory\" size=\"42\" placeholder=\"Enter Directory\">";
+        echo "<input type=\"submit\" name=\"submit\" value=\"Submit\">";
+        exit();
+    }
+
+    echo "Searching ".$url."<BR/><HR><BR/>\n";
+    search_directory($url);
+    
+    function search_directory($url)
+    {
+        $url = str_replace(array("\\", "//"), "/", $url."/");
+        
+        $i = 0;
+        if($open = opendir($url))
 	{
-		echo "$k => $item</BR>\n";
-	}
-	
-	echo "</BR>\n";
-	
-	$text = file_get_contents($url);
-	
-	$text = strip_tags($text);
-	
-	$arr = superExplode(strtolower($text), "() \\\n.\t;:,+\"|!?@#$%^&*~`=-_[]{}/<>");
-	
-	$disp = array_count_values($arr);
-	
-	ksort($disp);
-	
-	foreach($disp as $k=>$e)
-	{
-		echo "$k | $e</BR>\n";
-	}
-	
-	
-	function superExplode($str, $tok)
-	{
-		$i = 0;
-		
-		$arr[$i++] = strtok($str, $tok);
-		
-		while(($data = strtok($tok)) !== FALSE)
+            while(($elem = readdir($open)) !== FALSE)
+            {
+                if($elem != "." && $elem != "..")
 		{
-			$arr[$i++] = $data;
+                    if(is_dir($url."/".$elem))
+                    {
+                        $dirs[$i++] = $elem;
+                    }else if(is_file($url."/".$elem))
+                    {
+                        $files[$i++] = $elem;
+                    }
 		}
-		
-		return $arr;
+            }
+        
+            closedir($open);
+            
+            if(isset($files))
+            {
+                natcasesort($files);
+                foreach($files as $print)
+                {
+                    $file_info = pathinfo($url."/".$print);
+                    if(isset($file_info['extension']))
+                    {
+                        $file_info['extension'] = strtolower($file_info['extension']);
+                        if($file_info['extension'] === "html" | $file_info['extension'] === "htm")
+                        {
+                            echo $url.$print."<BR/>\n";
+                            page_info($url.$print);
+                            echo "<HR>\n";
+                        }
+                    }
+                }
+            }
+            
+            if(isset($dirs))
+            {
+                natcasesort($dirs);
+                
+                foreach($dirs as $print)
+                {
+                    search_directory($url.$print);
+                }
+            }
+        }
+    }
+        
+    function page_info($file)
+    {
+        $meta = get_meta_tags($file);
+        
+        foreach($meta as $k=>$item)
+        {
+            echo "$k => $item</BR>\n";
+        }
+	
+        echo "</BR>\n";
+
+        $text = strtolower(strip_tags(file_get_contents($file)));
+        
+        $arr = superExplode($text, "() \r\n.\t;:,+\"|!?@#$\'%^&*~`=-_[]{}/<>\\¶");
+        
+        $disp = array_count_values($arr);
+
+        ksort($disp);
+
+        foreach($disp as $k=>$e)
+        {
+            echo "$k | $e</BR>\n";
+        }
+    }
+	
+    function superExplode($str, $tok)
+    {
+	$i = 0;
+	
+	$arr[$i++] = strtok($str, $tok);
+	
+	while(($data = strtok($tok)) !== FALSE)
+	{
+		$arr[$i++] = $data;
 	}
+	
+	return $arr;
+    }
 ?>
