8c7157065c41390615b41b2cbeb79b3b00e5937f
Stephen Gaspar <evilgamerx42@gmail.com>
2013-10-07 20:22:32 -0400

    file browser v2, fully navigational directory structure

--- file_browser/file_browser.php
+++ file_browser/file_browser.php
@@ -1,11 +1,22 @@
 <?PHP
 	//I pledge my honour I have abided by the Stevens Honour Code. Stephen Gaspar
 
-	$path = "C:\www";
-	$d = array();
+        if(isset($_POST["Directory"]))
+        {
+            $path = $_POST["Directory"];
+        }else if(isset($_GET["path"]))
+        {
+            $path = $_GET["path"];
+        }else
+        {
+            echo "<form action=\"file_browser.php\" method=\"post\">";
+            echo "<input type=\"text\" name=\"Directory\" size=\"42\" placeholder=\"Enter Directory\">";
+            echo "<input type=\"submit\" name=\"submit\" value=\"Submit\">";
+            exit();
+        }
 	$files = array();
 	$i = 0;
-	
+        
 	if($open = opendir($path))
 	{
 		while(($elem = readdir($open)) !== FALSE)
@@ -21,22 +32,35 @@
 				}
 			}
 		}
-	}
-	
-	closedir($open);
-	
-	natcasesort($dirs);
-	natcasesort($files);
-	
-	echo "<a href=\" \">..</a></BR>\n";
-	
-	foreach($dirs as $print)
-	{
-		echo "<a href=\" \">$print</a></BR>\n";
-	}
-	
-	foreach($files as $print)
-	{
-		echo "$print</BR>\n";
-	}
+
+            closedir($open);
+             
+            $tmp = dirname($path)."/";
+            $temp = str_replace("//", "/", $tmp);
+            $temp = str_replace("\\", "/", $tmp);
+            echo "<a href=\"file_browser.php?path=$tmp\">..</a></BR>\n";
+
+                        
+            if(isset($dirs))
+            {
+                natcasesort($dirs);
+                
+                foreach($dirs as $print)
+                {
+                    $tmp = $path."/".$print."/";
+                    $tmp = str_replace("//", "/", $tmp);
+                    $tmp = str_replace("\\", "/", $tmp);
+                    echo "<a href=\"file_browser.php?path=$tmp\">$print</a></BR>\n";
+                }
+            }
+            
+            if(isset($files))
+            {
+                natcasesort($files);
+                foreach($files as $print)
+                {
+                    echo "$print</BR>\n";
+                }
+            }
+        }
 ?>
